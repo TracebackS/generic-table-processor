146ec9d1ff127fbca957c0d4586ef7747afa7495
traceback <673622006@qq.com>
2022-04-19 11:54:56 +0800

    add UT

--- src/data_represent.rs
+++ src/data_represent.rs
@@ -1,8 +1,6 @@
 use by_address::ByAddress;
 use std::cmp::{Ordering, PartialOrd};
-use std::collections::hash_map::DefaultHasher;
-use std::collections::HashMap;
-use std::collections::HashSet;
+use std::collections::{hash_map::DefaultHasher, HashMap, HashSet};
 use std::hash::{Hash, Hasher};
 
 /// Record's attributes, with it's type auto detected:
@@ -15,7 +13,7 @@ use std::hash::{Hash, Hasher};
 /// after, an error msg will be emitted
 ///
 /// It is small enough to be copyable
-#[derive(PartialEq, PartialOrd)]
+#[derive(PartialEq, PartialOrd, Debug)]
 pub enum Attr {
     Int(i32),
     Float(f32),
@@ -30,18 +28,18 @@ impl Attr {
             header
         )) {
             Attr::Int(_) => Attr::Int(raw_attr.parse::<i32>().expect(&format!(
-                "Error: expect int when parsing attribute {}, which value is {}",
+                "Error: expect int when parsing attribute `{}', which value is `{}'",
                 header, raw_attr
             ))),
             Attr::Float(_) => Attr::Float(raw_attr.parse::<f32>().expect(&format!(
-                "Error: expect float when parsing attribute {}, which value is {}",
+                "Error: expect float when parsing attribute `{}', which value is `{}'",
                 header, raw_attr
             ))),
             Attr::Bool(_) => Attr::Bool(match raw_attr {
                 "true" | "True" | "TRUE" | "t" | "T" => true,
                 "false" | "False" | "FALSE" | "f" | "F" => false,
                 _ => panic!(
-                    "Error: expect bool when parsing attribute {}, which value is {}",
+                    "Error: expect bool when parsing attribute `{}', which value is `{}'",
                     header, raw_attr
                 ),
             }),
@@ -378,9 +376,290 @@ pub struct FoldResult<'a> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::iter::zip;
+
+    fn make_a_ctx() -> Ctx {
+        let mut ctx = Ctx::new();
+        ctx.add_attr_type("userid", Attr::Int(0), Some(ComponentRule::Unique));
+        ctx.add_attr_type(
+            "time",
+            Attr::Float(0f32),
+            Some(ComponentRule::Interval(Interval { start: 1, step: 3 })),
+        );
+        ctx.add_attr_type("i", Attr::Int(0), None);
+        ctx.add_attr_type("f", Attr::Float(0f32), None);
+        ctx.add_attr_type("b", Attr::Bool(false), None);
+        ctx.add_attr_type("s", Attr::Str("hello".into()), None);
+        ctx
+    }
+
+    #[test]
+    fn construct_record() {
+        let headers = vec!["userid", "time", "i", "f", "b", "s"];
+        let raw_record_a = vec!["23", "2", "0", ".23", "true", "hello"];
+        let raw_record_b = vec!["23", "3", "8", ".45", "true", "world"];
+        let raw_record_c = vec!["24", "2", "1", ".25", "false", "yeah"];
+        let ctx = make_a_ctx();
+
+        let record_a = Record::new(
+            &ctx,
+            zip(headers.iter(), raw_record_a.into_iter())
+                .map(|(x, y)| (x.to_owned(), y))
+                .collect(),
+        );
+
+        assert_eq!(record_a.attrs.get("userid"), Some(&Attr::Int(23)));
+        assert_eq!(record_a.attrs.get("time"), Some(&Attr::Float(2f32)));
+        assert_eq!(record_a.attrs.get("i"), Some(&Attr::Int(0)));
+        assert_eq!(record_a.attrs.get("f"), Some(&Attr::Float(0.23f32)));
+        assert_eq!(record_a.attrs.get("b"), Some(&Attr::Bool(true)));
+        assert_eq!(record_a.attrs.get("x"), None);
+
+        let record_b = Record::new(
+            &ctx,
+            zip(headers.iter(), raw_record_b.iter())
+                .map(|(x, y)| (x.to_owned(), y.to_owned()))
+                .collect(),
+        );
+        let record_c = Record::new(
+            &ctx,
+            zip(headers.iter(), raw_record_c.iter())
+                .map(|(x, y)| (x.to_owned(), y.to_owned()))
+                .collect(),
+        );
+
+        assert_eq!(record_a.group_id, record_b.group_id);
+        assert_ne!(record_a.group_id, record_c.group_id);
+    }
+
+    #[test]
+    fn group_record() {
+        let headers = vec!["userid", "time"];
+        let ctx = make_a_ctx();
+        let record_group_a = vec![
+            vec!["0", "1.1"],
+            vec!["0", "2.9"],
+            vec!["0", "3"],
+            vec!["0", "3.9"],
+        ];
+        let record_group_b = vec![vec!["0", "7.9"], vec!["0", "9"]];
+        let record_group_c = vec![vec!["1", "4.1"], vec!["1", "6.9"]];
+
+        let get_group_state = |record_group: Vec<Vec<&str>>| -> (bool, Option<u64>) {
+            record_group
+                .into_iter()
+                .map(|raw_record| {
+                    Record::new(
+                        &ctx,
+                        zip(headers.iter(), raw_record.iter())
+                            .map(|(x, y)| (x.to_owned(), y.to_owned()))
+                            .collect(),
+                    )
+                })
+                .fold((true, None), |(is_same_group_id, group_id), x| {
+                    if let Some(group_id) = group_id {
+                        if is_same_group_id && x.group_id == group_id {
+                            (true, Some(x.group_id))
+                        } else {
+                            (false, Some(x.group_id))
+                        }
+                    } else {
+                        (true, Some(x.group_id))
+                    }
+                })
+        };
+
+        let (is_same_group_id_a, group_id_a) = get_group_state(record_group_a);
+        let (is_same_group_id_b, group_id_b) = get_group_state(record_group_b);
+        let (is_same_group_id_c, group_id_c) = get_group_state(record_group_c);
+        assert_eq!(is_same_group_id_a, true);
+        assert_eq!(is_same_group_id_b, true);
+        assert_eq!(is_same_group_id_c, true);
+        assert_ne!(group_id_a, group_id_b);
+        assert_ne!(group_id_b, group_id_c);
+        assert_ne!(group_id_c, group_id_a);
+    }
 
     #[test]
     fn construct_collection() {
-        assert_eq!(1, 1)
+        let headers = vec!["userid", "time"];
+        let ctx = make_a_ctx();
+        let records = vec![
+            // group_a
+            vec!["0", "1.1"],
+            vec!["0", "2.9"],
+            vec!["0", "3"],
+            vec!["0", "3.9"],
+            // group_b
+            vec!["0", "7.9"],
+            vec!["0", "9"],
+            // group_c
+            vec!["1", "4.1"],
+            vec!["1", "6.9"],
+        ];
+        let records: Vec<_> = records
+            .into_iter()
+            .map(|raw_record| {
+                Record::new(
+                    &ctx,
+                    zip(headers.iter(), raw_record.into_iter())
+                        .map(|(x, y)| (x.to_owned(), y))
+                        .collect(),
+                )
+            })
+            .collect();
+        let collection = Collection::new(records.iter().collect());
+        assert_eq!(collection.groups.len(), 3);
+    }
+
+    #[test]
+    fn filter_collection() {
+        let headers = vec!["userid", "time", "i"];
+        let ctx = make_a_ctx();
+        let records = vec![
+            vec!["0", "1", "233"],
+            vec!["0", "1", "23"],
+            vec!["0", "1", "2333"],
+            vec!["0", "1", "0"],
+            vec!["0", "1", "-28"],
+            vec!["0", "1", "233"],
+            vec!["0", "1", "366"],
+            vec!["0", "1", "243"],
+        ];
+        let records: Vec<_> = records
+            .into_iter()
+            .map(|raw_record| {
+                Record::new(
+                    &ctx,
+                    zip(headers.iter(), raw_record.into_iter())
+                        .map(|(x, y)| (x.to_owned(), y))
+                        .collect(),
+                )
+            })
+            .collect();
+        let collection = Collection::new(records.iter().collect());
+        let filter_cond = FilterCond {
+            attr_name: "i".into(),
+            val: Attr::Int(232),
+            ord: Ordering::Greater,
+        };
+        let collection = collection.filter_records(filter_cond);
+
+        assert_eq!(collection.groups.iter().next().unwrap().1.records.len(), 5);
+    }
+
+    #[test]
+    fn set_operations_on_collections() {
+        let headers = vec!["userid", "time", "i"];
+        let ctx = make_a_ctx();
+        let records = vec![
+            vec!["0", "1", "233"],
+            vec!["0", "1", "23"],
+            vec!["0", "1", "2333"],
+            vec!["0", "1", "0"],
+            vec!["0", "1", "-28"],
+            vec!["0", "1", "233"],
+            vec!["0", "1", "366"],
+            vec!["0", "1", "243"],
+        ];
+        let records: Vec<_> = records
+            .into_iter()
+            .map(|raw_record| {
+                Record::new(
+                    &ctx,
+                    zip(headers.iter(), raw_record.into_iter())
+                        .map(|(x, y)| (x.to_owned(), y))
+                        .collect(),
+                )
+            })
+            .collect();
+        let whole_view = records.iter().collect();
+        let end_with_3_view = records
+            .iter()
+            .filter(|record| {
+                if let Some(Attr::Int(x)) = record.attrs.get("i") {
+                    x % 10 == 3
+                } else {
+                    false
+                }
+            })
+            .collect();
+        let whole_view = Collection::new(whole_view);
+        let end_with_3_view = Collection::new(end_with_3_view);
+        let intersection = whole_view.intersection(&end_with_3_view);
+
+        assert_eq!(
+            intersection.groups.iter().next().unwrap().1.records.len(),
+            5
+        );
+
+        let union = intersection.union(&end_with_3_view);
+        assert_eq!(union.groups.iter().next().unwrap().1.records.len(), 5);
+
+        let difference = union.difference(&end_with_3_view);
+        assert_eq!(difference.groups.len(), 0);
+    }
+
+    #[test]
+    fn fold_collections() {
+        let headers = vec!["userid", "time", "i"];
+        let ctx = make_a_ctx();
+        let records = vec![
+            vec!["0", "1", "233"],
+            vec!["0", "1", "23"],
+            vec!["0", "1", "2333"],
+            vec!["0", "1", "0"],
+            vec!["0", "1", "-28"],
+            vec!["0", "1", "233"],
+            vec!["0", "1", "366"],
+            vec!["0", "1", "243"],
+        ];
+        let records: Vec<_> = records
+            .into_iter()
+            .map(|raw_record| {
+                Record::new(
+                    &ctx,
+                    zip(headers.iter(), raw_record.into_iter())
+                        .map(|(x, y)| (x.to_owned(), y))
+                        .collect(),
+                )
+            })
+            .collect();
+        let view = records.iter().collect();
+        let collection = Collection::new(view);
+
+        let count_result = collection.count();
+        assert_eq!(count_result.result.len(), 1);
+        assert_eq!(count_result.result.iter().next().unwrap().1, &Attr::Int(8));
+
+        let sum_result = collection.sum("i");
+        assert_eq!(
+            sum_result.result.iter().next().unwrap().1,
+            &Attr::Float(3403f32)
+        );
+
+        let avg_result = collection.avg("i");
+        assert_eq!(
+            avg_result.result.iter().next().unwrap().1,
+            &Attr::Float(425.375)
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "Error: header `x' is not found in context info")]
+    fn unexpected_header() {
+        let ctx = make_a_ctx();
+        let headers = ["userid", "time", "x"];
+        let record = vec!["0", "0", "0"];
+        Record::new(&ctx, zip(headers.into_iter(), record.into_iter()).collect());
+    }
+
+    #[test]
+    #[should_panic(expected = "Error: expect int when parsing attribute `i', which value is `true'")]
+    fn invalid_attr_type() {
+        let ctx = make_a_ctx();
+        let headers = ["userid", "time", "i"];
+        let record = vec!["0", "0", "true"];
+        Record::new(&ctx, zip(headers.into_iter(), record.into_iter()).collect());
     }
 }
